c364f01f6404ecdd75cc59a3da3b710830c263d8
Hiirigma <hariton1599@gmail.com>
2021-12-05 00:11:53 +0300

    Changes
    - Change input to file method (there is array at now);
    - lIdx - example's array. There are numbers near the Carmichael numbers with 3 to 8 prime factors.

--- karm_gen.py
+++ karm_gen.py
@@ -1,130 +1,53 @@
-import math
-import datetime
-import time
 import threading
+import os
+import datetime
 from mpmath.functions.functions import arg
 from sympy.ntheory import factorint
 
-def phi(n):
-	amount = 0        
-	for k in range(1, n + 1):
-		if math.gcd(n, k) == 1:
-			amount += 1
-	return amount
+G_D = 6
+G_MAX = 10000000000000000
 
-def is_prime(n):
-	""""pre-condition: n is a nonnegative integer
-	post-condition: return True if n is prime and False otherwise."""
-	if n < 2: 
-		 return False;
-	if n % 2 == 0:             
-		 return n == 2  # return False
-	k = 3
-	while k*k <= n:
-		 if n % k == 0:
-			 return False
-		 k += 2
-	return True
+file_array = []
 
-def GenCarmichaelNumbers(dPrimeCount : int):
-	numPrime = 1
-	dBlock = 10**3
+def GenCarmichaelNumbers(numPrime : int, maxNum : int):
+    start = datetime.datetime.now()
+    locPrime = numPrime
+    while numPrime < G_MAX:
+        numPrime += 2
+        lCarmichaelPrime = factorint(numPrime)
+        dLen = len(lCarmichaelPrime)
+        if (dLen < 3 or dLen > 8):
+            continue
 
-	while (numPrime):
-		# https://arxiv.org/pdf/1305.1867.pdf
-		# Corollary 2.16
+        # Corselt's Criterio
+        if (all((dCnt == 1 and (numPrime - 1) % (cPrime - 1) == 0) for cPrime, dCnt in lCarmichaelPrime.items())):
+            end = datetime.datetime.now()
+            file_array[dLen-3].write(f"time: {(end - start).seconds}: {numPrime} = {list(lCarmichaelPrime.keys())}" + os.linesep)
+            file_array[dLen-3].flush()
+            start = datetime.datetime.now()
 
-		# Carmichael numbers is odd 
-		sqr = math.isqrt(numPrime)
-		if (sqr * sqr == numPrime):
-			numPrime += 2
-			continue
-
-		if (is_prime(numPrime) == True):
-			numPrime+=2
-			continue	
-
-		lCarmichaelPrime = factorint(numPrime)
-
-		bCarmichael = True
-		for cPrime, dCnt in lCarmichaelPrime.items():
-			if (dCnt != 1):
-				bCarmichael = False
-				break
-			if ((numPrime - 1) % (cPrime - 1) != 0):
-				bCarmichael = False
-				break
-
-		if (bCarmichael == True and len(lCarmichaelPrime) == dPrimeCount):
-			if (len([*filter(lambda x: x >= dBlock, lCarmichaelPrime)]) == 0):
-				print(f"{numPrime} = {list(lCarmichaelPrime.keys())}")
-			else:
-				print (f"Number:{numPrime}. With 1000 factor")
-				return True
-		
-		numPrime += 2
-
-
-# def GenCarmichaelNumbers(numPrime : int):
-# 	# dIdx = numPrime
-# 	dThreadId = numPrime
-# 	numPrimeBlock = 100000 ** (numPrime + 1) + 1
-# 	numPrime = 100000 ** numPrime + 1
-
-# 	dBlock = 10**3
-# 	while (numPrime < numPrimeBlock):
-# 		start = datetime.datetime.now()
-# 		# https://arxiv.org/pdf/1305.1867.pdf
-# 		# Corollary 2.16
-# 		# Carmichael numbers is odd 
-# 		if (is_prime(numPrime) == True):
-# 			numPrime+=2
-# 			continue
-
-# 		k = 1
-# 		dSum = 0
-# 		bIsCarm = True
-
-# 		while k < numPrime:
-# 			# НОД = 1
-# 			if (math.gcd(k, numPrime) == 1):
-# 			# a^(n-1) 	
-# 				dPow = pow(k, numPrime - 1, numPrime)
-# 				dSum += dPow
-# 				if (dPow != 1):
-# 					bIsCarm = False
-# 					break
-# 			k += 1
-
-# 		if (bIsCarm == True):
-# 			if (dSum == phi(numPrime)):
-# 				lCarmichaelPrime = factorint(numPrime, multiple=True)
-# 				if (len([*filter(lambda x: x >= dBlock, lCarmichaelPrime)]) == 0):
-# 					end = datetime.datetime.now()
-# 					print (f"{dThreadId}: Carmichael number is: {numPrime} = {lCarmichaelPrime}")
-# 					print(f"{dThreadId}: Time: {(end - start)}")
-# 				else:
-# 					print (f"{dThreadId}: Number:{numPrime}. With 1000 factor")
-
-# 		numPrime += 2
-
-# 	return True
+    
+    print(f'end for {numPrime}')
 
 def main():
-	print(f'Generate Carmichael numbers')
-	threads = list()
-	# for dIdx in range(5):
-	# 	time.sleep(1)
-	# 	t1 = threading.Thread(target=GenCarmichaelNumbers, args=(dIdx,))
-	# 	threads.append(t1)
-	# 	t1.start()
+    print('Generate Carmichael numbers')
 
-	# for t1 in threads:
-	# 	t1.join()
+    for i in range(3,9):
+        file_array.append(open(f'{i}.txt', 'w'))
 
-	GenCarmichaelNumbers(3)
-	return 0
+    threads = list()
+    dPrimeCount = 3
+    lIdx = [1,4 * (10 ** 4) + 1,8 * (10 ** 4) + 1, 321197185 - 2, 5394826801 - 2, 232250619601 - 2, 10**19 + 1, 10**22 + 1]
+    while dPrimeCount != 9:
+        t1 = threading.Thread(target=GenCarmichaelNumbers, args=(lIdx[dPrimeCount - 3],lIdx[dPrimeCount - 2],))
+        threads.append(t1)
+        t1.start()
+        dPrimeCount+=1
 
+    for t1 in threads:
+        t1.join()
+
+    return 0
 
 if __name__ == "__main__":
-	main()
+    main()
